036d0f29c60090e39b22f9845c21ccb3d82b9069
adam ross russell and emmanuel cutler <adamrr724@yahoo.com>
2016-01-11 16:16:09 -0800

    add styling and new java next function

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,61 @@
 $(function() {
-    $(".java").click(function() {
-      $(".java .initially-hidden").slideToggle();
-    });
-    $(".string").click(function() {
-      $(".string .initially-hidden").slideToggle();
-    });
-    $(".modulo").click(function() {
-      $(".modulo .initially-hidden").slideToggle();
-    });
-    $(".operators").click(function() {
-      $(".operators .initially-hidden").slideToggle();
-    });
-    $(".nan").click(function() {
-      $(".nan .initially-hidden").slideToggle();
-    });
-    $(".variable").click(function() {
-      $(".variable .initially-hidden").slideToggle();
-    });
-    $(".plus").click(function() {
-      $(".plus .initially-hidden").slideToggle();
-    });
-    $(".function").click(function() {
-      $(".function .initially-hidden").slideToggle();
-    });
-    $(".method").click(function() {
-      $(".method .initially-hidden").slideToggle();
-    });
-    $(".argument").click(function() {
-      $(".argument .initially-hidden").slideToggle();
-    });
-    $(".chaining").click(function() {
-      $(".chaining .initially-hidden").slideToggle();
-    });
-    $(".escape").click(function() {
-      $(".escape .initially-hidden").slideToggle();
-    });
-    $(".aler").click(function() {
-      $(".aler .initially-hidden").slideToggle();
-    });
-    $(".commenting").click(function() {
-      $(".commenting .initially-hidden").slideToggle();
-    });
-    $(".booleans").click(function() {
-      $(".booleans .initially-hidden").slideToggle();
-    });
-    $(".undefined").click(function() {
-      $(".undefined .initially-hidden").slideToggle();
-    });
-    $(".parameter").click(function() {
-      $(".parameter .initially-hidden").slideToggle();
-    });
-    $(".return").click(function() {
-      $(".return .initially-hidden").slideToggle();
-    });
+  $(".title").click(function(){
+    $(this).next().slideToggle();
+  });
+    // $(".java").click(function() {
+    //   $(".java .initially-hidden").slideToggle();
+    // });
+    // $(".string").click(function() {
+    //   $(".string .initially-hidden").slideToggle();
+    // });
+    // $(".modulo").click(function() {
+    //   $(".modulo .initially-hidden").slideToggle();
+    // });
+    // $(".operators").click(function() {
+    //   $(".operators .initially-hidden").slideToggle();
+    // });
+    // $(".nan").click(function() {
+    //   $(".nan .initially-hidden").slideToggle();
+    // });
+    // $(".variable").click(function() {
+    //   $(".variable .initially-hidden").slideToggle();
+    // });
+    // $(".plus").click(function() {
+    //   $(".plus .initially-hidden").slideToggle();
+    // });
+    // $(".function").click(function() {
+    //   $(".function .initially-hidden").slideToggle();
+    // });
+    // $(".method").click(function() {
+    //   $(".method .initially-hidden").slideToggle();
+    // });
+    // $(".argument").click(function() {
+    //   $(".argument .initially-hidden").slideToggle();
+    // });
+    // $(".chaining").click(function() {
+    //   $(".chaining .initially-hidden").slideToggle();
+    // });
+    // $(".escape").click(function() {
+    //   $(".escape .initially-hidden").slideToggle();
+    // });
+    // $(".aler").click(function() {
+    //   $(".aler .initially-hidden").slideToggle();
+    // });
+    // $(".commenting").click(function() {
+    //   $(".commenting .initially-hidden").slideToggle();
+    // });
+    // $(".booleans").click(function() {
+    //   $(".booleans .initially-hidden").slideToggle();
+    // });
+    // $(".undefined").click(function() {
+    //   $(".undefined .initially-hidden").slideToggle();
+    // });
+    // $(".parameter").click(function() {
+    //   $(".parameter .initially-hidden").slideToggle();
+    // });
+    // $(".return").click(function() {
+    //   $(".return .initially-hidden").slideToggle();
+    // });
+    //
+
 });
